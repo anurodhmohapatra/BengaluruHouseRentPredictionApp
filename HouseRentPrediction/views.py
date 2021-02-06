7ca29b66aa16a8d42e1fa2284be3ece8b2e32579
Anurodh Mohapatra <anurodhmohapatra@gmail.com>
2021-02-06 14:40:27 +0530

    first commit

---
+++ HouseRentPrediction/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import pickle
+import json
+import pandas as pd
+
+# load the model from disk
+model = pickle.load(open('model.pkl', 'rb'))
+map_add = json.load(open('map.json', 'r'))
+
+
+def predict(Address, Size):
+    X = pd.DataFrame([[Address, Size]], columns=['Address', 'Size(Acres)'])
+    X['Address'] = X['Address'].map(map_add)
+    prediction = model.predict(X)
+    return int(prediction)
+
+
+def home(request):
+    params = {'add': list(map_add.keys())}
+    return render(request, 'HouseRentPrediction/index.html', params)
+
+
+def index(request):
+    address = str(request.POST.get('address', 'others'))
+    size = int(request.POST.get('size', '500'))
+    pred = predict(address, size)
+    params = {'add': list(map_add.keys()),
+              'pred': f"Monthly Rent for {size} sqft in {address} is Rs {pred}"}
+    return render(request, 'HouseRentPrediction/index.html', params)
